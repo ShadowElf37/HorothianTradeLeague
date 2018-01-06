8176d5bb7c5dafb8542f15f1b1ea7578e291d64c
ShadowElf37 <ykey-cohen@emeryweiner.org>
2018-01-05 18:10:11 -0600

    Ready for development

--- server.py
+++ server.py
@@ -30,6 +30,7 @@ class Server:
 
         self.connection = None
         self.c_address = ['', 0]
+        self.handled_counter = 0
 
         # On-board data management if needed
         self.state = {}
@@ -40,11 +41,41 @@ class Server:
 
     def send(self, msg):
         try:
-            self.connection.send(msg.encode(ENCODING))
+            if type(msg) != type(bytes()):
+                self.connection.send(msg.encode(ENCODING))
+            else:
+                self.connection.send(msg)
         except AttributeError:
             print("Error: tried to send with no client connected")
             return 1
         return 0
+
+    def send_file(self, faddr):
+        ext1 = faddr[-3:]
+        ext2 = faddr[-4:]
+        ext3 = faddr[-5:]
+        if ext2 in ('.png', '.jpg', '.gif', '.ico',) or ext3 in ('.jpeg',):
+            faddr = self.get_image(faddr)
+        elif ext2 in ('.htm',) or ext3 in ('.html',):
+            faddr = self.get_page(faddr)
+        elif ext2 in ('.css',) or ext1 in ('.js',):
+            faddr = self.get_script_or_style(faddr)
+
+        f = open(faddr, 'rb')
+        self.send(f.read())
+        f.close()
+
+    @classmethod
+    def get_image(cls, fname):
+        return 'web/images/%s' % fname
+
+    @classmethod
+    def get_page(cls, fname):
+        return 'web/%s' % fname
+
+    @classmethod
+    def get_script_or_style(cls, fname):
+        return 'web/scripts_and_stylesheets/%s' % fname
 
     def recv(self):
         try:
@@ -57,13 +88,33 @@ class Server:
         self.socket.listen(1)
         while True:
             self.connection, self.c_address = self.socket.accept()
-            self.handle_request(self.connection, self.c_address, self.parse(self.recv()))
+            self.handle_request(self, self.connection, self.c_address, self.parse(self.recv()))
+            self.handled_counter += 1
 
     def parse(self, request):
-        request = request.split(' ')[1][1:]  # reduces GET xx HTTP to xx
-        request.split('/')
-        request.remove('')
+        request = request.split(' ')  # reduces GET xx HTTP to xx
+        request = [request[0], request[1][1:]]
+        request[1].split('/')
         return request
 
-    def handle_request(self, conn, addr, req):  # Should be set in main.py
+    def set_request_handler(self, func):
+        self.handle_request = func
+
+    def handle_request(self, IGNORE_THIS_PARAMETER, conn, addr, req):  # Should be set in main.py
         return 0
+
+if __name__ == "__main__":
+    s = Server()
+
+    def handle(self, conn, addr, req):
+        print(req)
+        if req[1] == '':
+            self.send_file("home.html")
+        else:
+            self.send_file(req[1])
+        print("Connection!", addr)
+        conn.close()
+
+    s.set_request_handler(handle)
+    print("Server open, listening...")
+    s.open()
