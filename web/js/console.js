238749b36589b2b1fdb1a1ca99647162142b350f
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2018-03-15 21:13:50 -0500

    implement command history

--- web/js/console.js
+++ web/js/console.js
@@ -13,6 +13,40 @@ var IOCN_TYPE = "span";
   * the console has output functions print() and println(), pass UTF-8 strings please
 ***/
 
+function CHistory(max) {
+    this.ohistory = [];
+    this.history = [''];
+    this.pos = 0;
+    this.max = max;
+}
+CHistory.prototype.up = function(old) {
+    console.log(this.history, this.pos);
+    if(this.pos == 0)
+        return old;
+    this.history[this.pos--] = old;
+    console.log(this.history, this.pos);
+    return this.history[this.pos];
+}
+CHistory.prototype.down = function(old) {
+    console.log(this.history, this.pos);
+    if(this.pos == this.history.length)
+        return old;
+    this.history[this.pos++] = old;
+    console.log(this.history, this.pos);
+    return this.history[this.pos];
+}
+CHistory.prototype.commit = function(cmd) {
+    if(!cmd.match(/^\s/) && cmd !== this.ohistory[this.ohistory.length - 1])
+    {
+        this.ohistory.push(cmd);
+        if(this.ohistory.length > this.max)
+            this.ohistory.shift();
+        this.history = Array.from(this.ohistory);
+        this.history.push("");
+    }
+    this.pos = this.ohistory.length;
+}
+
 function IOConsole(caller, outnode, container) {
     if(!outnode)
         throw "Invalid outnode!";
@@ -24,9 +58,10 @@ function IOConsole(caller, outnode, container) {
     this.input = document.createElement(IOCN_TYPE);
     this.input.contentEditable = "true";
     this.input.autofocus = true;
-    this.input.placeholder = "fsck";
     this.input.focus();
     this.outnode.appendChild(this.input);
+    
+    this.history = new CHistory(1000);
     
     var self = this;
     window.addEventListener('keypress', function(ev) {
@@ -44,16 +79,24 @@ IOConsole.prototype.onkey = function(ev) {
     if(!this.container.contains(ev.target))
         return false;
     var text = this.input.innerText;
-    var args = text.replace(/^\s+|\s+$/g, '').split(/\s+/);
+    var args = text.replace(/^\s+|\s+$/g, "").split(/\s+/);
     var cmd = args.shift();
     
     switch(ev.key) {
     case "Enter":
         this.println(text);
-        this.caller.call(cmd, args);
+        this.history.commit(text);
+        if(cmd)
+            this.caller.call(cmd, args);
         this.print(this.caller.prompt());
-        this.input.innerHTML = '';
+        this.input.innerHTML = "";
         ev.preventDefault();
+        break;
+    case "ArrowDown":
+        this.input.innerText = this.history.down(text);
+        break;
+    case "ArrowUp":
+        this.input.innerText = this.history.up(text);
         break;
     default:
         console.log(ev.key);
@@ -81,5 +124,5 @@ IOConsole.prototype.print = function(str) {
     this.outnode.insertBefore(child, this.input);
 }
 IOConsole.prototype.println = function(str) {
-    this.print((str || '') + '\n');
+    this.print((str || "") + '\n');
 }
