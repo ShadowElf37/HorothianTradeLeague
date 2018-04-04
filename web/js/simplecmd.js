3303666c87e926db3200f8ef7d5c8e6ccba6c042
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2018-04-04 18:45:03 -0500

    fIX YOUR DAMNG AHTIANGA AJIGTIAI HA IJ AOVELLLL

--- web/js/simplecmd.js
+++ web/js/simplecmd.js
@@ -21,40 +21,25 @@ SimpleCmd.prototype.console = function(o) {
 SimpleCmd.prototype.def = function(cmd, args) {
     this.output.println("Unknown command " + cmd + ". " + this.helpstr);
 }
-SimpleCmd.prototype.call = function(cmd, args, input) {
-    this.waiting = true;
+SimpleCmd.prototype.call = function(cmd, args) {
     var func = this.functions[cmd];
     var self = this;
-    this.input = input;
-    if(func) {
-        self.output.println(cmd + ' ' + args.join(' '));
+    if(func)
         func.call(this, args);
-        self.output.print(self.prompt());
-        self.input.contentEditable = "true";
-        self.input.innerHTML = "&#8203;";
-        self.input.focus();
-        }
-    else {
-        this.input.contentEditable = "false";
+    else
         fetch("http://[[host]]:[[port]]/cmd/" + cmd + '/' + args.join('-')).then(function(response) {
             response.text().then(function(text) {
                 t = text.split('|')
                 style = t[0];
                 color = t[1];
                 text = t.slice(2).join('|').split('\n');
-                self.output.println(cmd + ' ' + args.join(' '));
                 self.output.format(style, color);
                 for (i=0; i<text.length; i++){
                     self.output.println(text[i]);
                 }
                 self.output.clrfmt();
-                self.output.print(self.prompt());
-                self.input.contentEditable = "true";
-                self.input.innerHTML = "&#8203;";
-                self.input.focus();
+                self.output.finish();
             });
         });
-
-    }
         //this.def(cmd, args);
 }
